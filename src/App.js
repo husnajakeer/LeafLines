bcfe17d9cf8f67ce397bd4fdf33ca81e78593b52
Saniya Savla <ssavla30@gatech.edu>
2023-10-14 01:11:45 -0400

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/navbar';
-
+import CountrySelector from './components/selector/selector';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <CountrySelector/>
     </div>
   );
 }
